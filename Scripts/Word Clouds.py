5adb36f702ea17ce7c7dab1e1a0a242ea8f4dacf
vmagdale2 <jobsearchvmagda@gmail.com>
2025-01-12 17:41:29 -0600

    Added 6 Word Clouds

--- Scripts/Word Clouds.py
+++ Scripts/Word Clouds.py
@@ -2,17 +2,17 @@ dataset_table = "UPDATE WITH TABLE"
 
 query = f"""
 SELECT
-  Gen_Feed
+  Gen_Feed # UPDATE WITH COLUMN
 FROM
   `{project_id}.{dataset_table}`
 WHERE
-  Gen_Feed IS NOT NULL
+  Gen_Feed IS NOT NULL # UPDATE WITH SAME COLUMN
 """
 
 query_job = client.query(query)
 results = query_job.result()
 
-responses = [row.Gen_Feed for row in results]
+responses = [row.Gen_Feed for row in results] # UPDATE WITH SAME COLUMN
 text_data = " ".join(responses)
 
 wordcloud = WordCloud(
@@ -26,5 +26,5 @@ wordcloud = WordCloud(
 plt.figure(figsize=(10, 5))
 plt.imshow(wordcloud, interpolation="bilinear")
 plt.axis("off")
-plt.title("Word Cloud for [Insert Topic Here]", fontsize=20)
+plt.title("Word Cloud for [Insert Topic Here]", fontsize=20) # UPDATE WITH TOPIC
 plt.show()
